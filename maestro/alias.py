a5e0b6310f03b14a593026274f3d2a8a3f0c1d06
Max Moon <moon.maxwell@gmail.com>
2017-09-02 13:03:40 -0700

    Adding working alias creation
    This change adds a working alias creation method. When creating an alias it prompts the user for an alias name, prints out the list of avail versions then creates the alias.
    
    I also moved alias creation to it's own file for readability reasons.

---
+++ maestro/alias.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python2.7
+
+import boto3
+import lambda_config
+import sys
+import json
+import zipfile
+import os
+from botocore.exceptions import ClientError
+
+DOC = sys.argv[2]
+client = boto3.client('lambda')
+iam = boto3.resource('iam')
+roles = boto3.client('iam')
+
+def json_parser():
+  with open('%s' % DOC) as json_data:
+    read = json.load(json_data)
+    return read
+    return True
+  print "No json document to read.. Please enter a valid json document"
+
+def alias_creation():
+  lambda_name = json_parser()["initializers"]["name"]
+
+  get_alias_name = raw_input("What would you like this alias to be called? ")
+  alias_name = get_alias_name.lower()
+
+  versions = client.list_versions_by_function(
+                    FunctionName='%s' % lambda_name,
+                  )
+
+  version_json = json.dumps(versions, indent=4)
+  load_json = json.loads(version_json)
+  versions = load_json['Versions']
+  avail_versions = []
+
+  for version in versions:
+    if version['Version'] != 0:
+      print('version: %s' % version['Version'])
+      avail_versions.append(version['Version'])
+
+  function_version = raw_input("What version would you like to create an alias for? ")
+  
+  if function_version in avail_versions: 
+    try:
+      add_alias = client.create_alias(
+        FunctionName='%s' % lambda_name,
+        Name='%s' % alias_name,
+        FunctionVersion='%s' % function_version,
+      )
+      return True
+    except ClientError, message:
+      print message
+  else:
+    print "I can't find that version, check list and find again"
+
+alias_creation()
