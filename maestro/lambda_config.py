b39dde5a8fe4de09e206a6c708378f2db6c4db19
Max Moon <mmoon@tunein.com>
2017-09-12 11:34:08 -0700

    Adding cloudwatch events methods
    This push also removes the ‘sqs’ from available principals, as sqs
    needs to tie in with sns or cloudwatch events to integrate with Lambda

--- maestro/lambda_config.py
+++ maestro/lambda_config.py
@@ -2,4 +2,4 @@ AVAIL_RUNTIMES = ['nodejs4.3', 'nodejs6.10', 'java8', 'python2.7', 'python3.6', 
 AVAIL_ACTIONS = ['create', 'update-code', 'update-config','delete', 'publish', 'create-alias', 'delete-alias', 'update-alias']
 REGIONS = ['eu-west-1', 'us-west-1', 'us-west-2', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2','ap-northeast-1', 'sa-east-1', 'cn-north-1', 'eu-central-1']
 ACL_ANSWERS = ['private', 'public-read', 'public-read-write', 'authenticated-read']
-PRINCIPALS = ['s3', 'sqs', 'sns', 'cloudwatch']
+PRINCIPALS = ['s3', 'sns', 'cloudwatch']
--- maestro/triggers.py
+++ maestro/triggers.py
@@ -16,6 +16,7 @@ client = boto3.client('lambda')
 s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
 sns_client = boto3.client('sns')
+cloudwatch_client = boto3.client('events')
 
 my_session = boto3.session.Session()
 region = my_session.region_name
@@ -32,8 +33,8 @@ def json_parser():
   print("No json document to read.. Please enter a valid json document")
 '''
 
-invoke_method = 'sns'
-invoke_source = 'Test-topic'
+invoke_method = 'cloudwatch'
+invoke_source = 'lambda-cloudwatch-event'
 
 def check_s3():
   bucket_name = invoke_source #json_parser()['backup']['bucket_name']
@@ -69,11 +70,22 @@ def get_sns_arn():
   except ClientError as error:
     print(json.dumps(error.response, indent=4))
 
-def get_sqs_arn():
-  print("Here is where we get the info for sqs")
-
 def get_cloudwatch_arn():
-  print("Here is where we get the info for cloudwatch")
+    event_name = invoke_source
+
+    if len(event_name)>0:
+      try:
+        get_events = cloudwatch_client.list_rules(NamePrefix=event_name)
+        
+        dumper = json.dumps(get_events, indent=4)
+        loader = json.loads(dumper)
+        if loader['ResponseMetadata']['HTTPStatusCode'] == 200:
+          rules = loader['Rules']
+          for rule in rules:
+              return rule['Arn']
+
+      except ClientError as error:
+        print(error.response['Error']['Message'])
 
 def get_aliases():
   print("Here is where you grab avail aliases")
@@ -86,15 +98,15 @@ def add_invoke_permission():
     if invoke_method == 's3':
       principal = 's3.amazonaws.com'
       source_arn = "%s/*" % get_s3_arn()
-    if invoke_method == 'sqs':
-      principal = 'sqs.amazonaws.com'
-      source_arn = get_sqs_arn()
     if invoke_method == 'sns':
       principal = 'sns.amazonaws.com'
       source_arn = get_sns_arn()
     if invoke_method == 'cloudwatch':
       principal = 'events.amazonaws.com'
       source_arn = get_cloudwatch_arn()
+    else:
+      print("No valid principal and source arn found.. breaking")
+      sys.exit[1]
 
     if len(principal)>0:
       if len(source_arn)>0:
@@ -171,13 +183,20 @@ def invoke_action():
         print(error.response['Error']['Message'])
         sys.exit[1]
 
-    if invoke_method == 'sqs':
-      print("Still figuring this out")
-
     if invoke_method == 'cloudwatch':
-      print("Still figuring this out")
-
-invoke_action()
+      rule = invoke_source
+      try:
+        add_target = cloudwatch_client.put_targets(
+                      Rule=rule,
+                      Targets=[
+                        {
+                        'Id': lambda_name,
+                        'Arn': arn,
+                        }
+                      ]
+                    )
+      except ClientError as error:
+        print(error.response['Error']['Message'])
 
 def remove_invoke_action():
   lambda_name = json_parser()['initializers']['name']
