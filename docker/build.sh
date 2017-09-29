fec1cba70743d53e6f5efe7bc5ee541c36d85325
Max Moon <mmoon@tunein.com>
2017-09-29 00:30:12 -0700

    fixing build script

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,7 @@
 
 aws s3 cp s3://ti-devops-test-dockerfiles/Maestro/Dockerfile.gocompile
 
+build () {
 if [ -f Dockerfile ]; then
     docker build -f Dockerfile.gocompile . -t go-build-container-$lambda_name \
             --build-arg maestro_token=$maestro_token \
@@ -9,13 +10,34 @@ if [ -f Dockerfile ]; then
             --build-arg secret_key=$AWS_SECRET_ACCESS_KEY \
             --build-arg region=$region \
             --build-arg app=$app
+    true
 else
     echo "No build file found"
+    false
 fi
+}
 
+run () {
+check_images=$(docker images maestro-build-container-$lambda_name)
+if [ $? -eq 0 ]; then
+  docker run go-build-container-$lambda_name
+  true
+else
+  echo "false"
+  false
+fi
+}
+
+remove () {
 check_running=$(docker ps -f name=go-build-container-$lambda_name | grep Up | awk '{ print $1 }')
 if [ $? -eq 1 ]; then
   echo "The container is still running.."
 else
   docker rmi --force go-build-container-$lambda_name
+  true
 fi
+}
+
+if build; then
+	if run; then
+		remove
