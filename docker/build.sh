32ba87a54fe7540d8e6273845ec868682db7ec25
Max Moon <mmoon@tunein.com>
2017-09-29 11:56:14 -0700

    killing variables

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-maestro_token=%env.MAESTRO_GIT_TOKEN%
-AWS_ACCESS_KEY_ID=%env.AWS_ACCESS_KEY_ID%
-AWS_SECRET_ACCESS_KEY=%env.AWS_SECRET_ACCESS_KEY%
-environment=%env.environment%
-region=%env.region%
-
 ##Build dat shit
 docker run -v `pwd`:/app -w /app/helloworld --entrypoint /bin/bash microsoft/dotnet -c 'dotnet restore && dotnet build -o dist'
