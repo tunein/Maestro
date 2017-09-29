fbb39607dc331a7e6d42811cbaeb49e43fa23f48
Max Moon <mmoon@tunein.com>
2017-09-29 00:26:01 -0700

    adding grabbing the compiled binary from s3

--- docker/update_code_no_pub.sh
+++ docker/update_code_no_pub.sh
@@ -15,9 +15,13 @@ region=%env.region%
 app=%env.APP_NAME%
 lambda_name=%env.lambda_name%
 
-######### LETS START! #############
-#cd $app
+mkdir -p app
 
+######### LETS START! #############
+##Grab the packaged code
+aws s3 cp s3://ti-devops-lambda-files/$lambda_name/app/*  /app
+
+##Get the deploy file
 aws s3 cp s3://ti-devops-test-dockerfiles/Maestro/Dockerfile
 
 if [ -f Dockerfile ]; then
