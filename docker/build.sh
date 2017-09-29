c8fa9d81864de463cb128a0d6e90f5ca45b49a72
Max Moon <mmoon@tunein.com>
2017-09-29 11:55:43 -0700

    removing comment

--- docker/build.sh
+++ docker/build.sh
@@ -6,9 +6,5 @@ AWS_SECRET_ACCESS_KEY=%env.AWS_SECRET_ACCESS_KEY%
 environment=%env.environment%
 region=%env.region%
 
-
-####
-# VCS Shit
-
 ##Build dat shit
 docker run -v `pwd`:/app -w /app/helloworld --entrypoint /bin/bash microsoft/dotnet -c 'dotnet restore && dotnet build -o dist'
