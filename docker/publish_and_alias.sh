efc4d0d477d1deddf447b892ccacba37acae75d0
Max Moon <mmoon@tunein.com>
2017-09-28 23:05:40 -0700

    Adjusting build and release scripts

---
+++ docker/publish_and_alias.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+environment=%env.environment%
+region=%env.region%
+lambda_name=%env.lambda_name%
+git_sha=git_sha
+
+publish_code () {
+  check_images=$(docker images maestro-build-container-$lambda_name)
+  if [ $? -eq 0 ]; then
+    echo "Publishing version"
+    docker run maestro-build-container-$lambda_name publish --version_description $git_sha $environment.$region.json
+    true
+  else
+    echo "No container found"
+    false
+  fi
+}
+
+update_alias () {
+  check_images=$(docker images maestro-build-container-$lambda_name)
+  if [ $? -eq 0 ]; then
+    echo "Updating alias"
+    docker run maestro-build-container-$lambda_name update-alias --publish $environment.$region.json
+    true
+  else
+    echo "No container found"
+    false
+  fi
+}
+
+if publish_code; then 
+  update_alias
+fi
