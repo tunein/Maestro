903053ee3781b95d1a256c371af22e9a0ae6b2e2
Max Moon <mmoon@tunein.com>
2017-10-03 12:09:15 -0700

    updating deploy scripts

--- deploy-scripts/update_code_no_pub.sh
+++ deploy-scripts/update_code_no_pub.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -e AWS_ACCESS_KEY_ID='%env.AWS_ACCESS_KEY_ID%' -e AWS_SECRET_ACCESS_KEY='%env.AWS_SECRET_ACCESS_KEY%' -e AWS_DEFAULT_REGION='%env.region%' -v `pwd`:/app tunein/maestro-alpine:0.1.1 update-code --no_pub %env.environment%.%env.region%.json
+docker run --rm -e AWS_ACCESS_KEY_ID='%env.AWS_ACCESS_KEY_ID%' -e AWS_SECRET_ACCESS_KEY='%env.AWS_SECRET_ACCESS_KEY%' -e AWS_DEFAULT_REGION='%env.region%' -v `pwd`:/app tunein/maestro-alpine:0.1.1 update-code --no_pub %env.account%.%env.region%.%env.environment%.json
