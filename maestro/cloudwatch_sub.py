02e3afcc4377ceb85114801c41cfdd4e365427b0
Max Moon <moon.maxwell@gmail.com>
2017-11-10 11:00:04 -0800

    adding inline documentation
    part of the reformatting effort is that each function has documentation, for both myself and other readers of the code/collaborators

--- maestro/cloudwatch_sub.py
+++ maestro/cloudwatch_sub.py
@@ -1,21 +1,37 @@
+#External libs
 import json
 import boto3
 from botocore.exceptions import ClientError
 
+#Establish our boto resources
 logClient = boto3.client('logs')
 lambdaClient = boto3.client('lambda')
 iam = boto3.resource('iam')
 
-#Invoke the function, creating the log group
 def invokeFunction(name):
+    '''
+    Invokes the new function, AWS will generate a log group and stream
+    We don't/shouldn't care about the output or if the lambda works, just for the lambda to generate a log group
+
+    args:
+        name: name of the lambda, retrieved from config file
+    '''
     print('Creating log group')
     try:
         invoke = lambdaClient.invoke(FunctionName=name)
     except ClientError as error:
         print(error)
 
-#Get the group name of the new lambda, this is more of a check to see if it exists
 def getLogGroupName(name):
+    '''
+    Gets the unique amazon resource name (ARN) for the log group we are creating with 'invokeFunction'
+    Accomplishes this by searching existing log groups for the name of the function
+
+    Note: All Lambda generated log groups are prefixed with '/aws/lambda', as seen below
+    
+    args:
+        name: the name of the lambda function, retried from config file
+    '''
     print('Getting lambda log group name')
     try:
         logGroupName = logClient.describe_log_groups(
@@ -34,8 +50,16 @@ def getLogGroupName(name):
     except ClientError as error:
         print(error)
 
-#Get the arn of the logging lambda
 def getDestinationArn(destLambda, destAlias):
+    '''
+    Retrieves the unique amazon resource name (ARN) for the destination lambda
+    This unique id is required to accomplish the function below (putSubFilter)
+    We need the alias because each alias has it's own arn (if you're using one)
+
+    args:
+        destLambda: name of the processing lambda
+        destAlias: name of the alias on the processing lambda
+    '''
     print('Getting destination arn for logging lambda')
     try:
         destinationArn = lambdaClient.get_function(
@@ -52,8 +76,18 @@ def getDestinationArn(destLambda, destAlias):
     except ClientError as error:
         print(error)
 
-#Add subscription filter to new lambda to pipe logs to logging lambda
 def putSubfilter(groupName, arn, role, name):
+    '''
+    Adds subscription filter to new lambda to pipe logs to logging lambda
+
+    Takes info from 'getLogGroupName', 'getDestinationArn', then the role and new lambda name from the config file
+
+    args:
+        groupName: name retrieved by 'getLogGroupName'
+        arn: the amazon resource name (ARN) for the destination lambda
+        role: not sure why this is still here, i probably forgot to remove it, likely removeable?
+        name: the name of the new lambda, from the config file, used to create a unique filterName for the sub filter
+    '''
     print('Adding subscription filter to logging lambda')
     try:
         putFilter = logClient.put_subscription_filter(
@@ -65,8 +99,18 @@ def putSubfilter(groupName, arn, role, name):
     except ClientError as error:
         print(error)
 
-#Add the ability for the new lambda logs to invoke the logging lambda
 def putInvokePerm(destLambda, destAlias, name, region, logArn):
+    '''
+    Adds the ability for the new lambda logs to invoke the logging lambda
+    This can be seen in the triggers of the destination lambda
+
+    args:
+        destLambda: name of the destination lambda, retrieved from config
+        destAlias: alias on the destination lambda, retrieved from config
+        name: name of the new lambda, retrieved from config, used to create a statement id
+        region: region this is happening in, used for the principal, seen below
+        logArn: arn of the new lambda log group, retried from 'getLogGroupName'
+    '''
     print('Adding invoke permission to logging lambda')
     try:
         addPermission = lambdaClient.add_permission(
@@ -80,8 +124,13 @@ def putInvokePerm(destLambda, destAlias, name, region, logArn):
     except ClientError as error:
         print(error)
 
-#Get role arn
 def getRoleArn(role):
+    '''
+    Get unique amazon resource name (ARN) of the role, this is used in 'putInvokePerm' function
+
+    args:
+        role: name of the role you're using, retrieved from config
+    '''
     print('Gathering role arn')
     try:
         role = iam.Role('name')
@@ -92,6 +141,18 @@ def getRoleArn(role):
 
 ########## Main Entrypoint ############
 def cloudwatchSubscription(newLambdaName, destLambdaName, destLambdaAlias, region, role):
+    '''
+    To simplify usage of this module This is the main entrypoint that calls all above modules
+
+    I've debated putting the above in a class but it seems like overkill to accomplish what is already working
+
+    args:
+        new_lambda_name: name the lambda we're creating, retrieved from config file
+        dest_lambda_name: name of lambda processing our logs, retrieved from config file
+        dest_lambda_alias: name of the ALIAS of the lambda processing our logs, retrieved from config file
+        region: region this will occur in, retried from config file
+        role: a valid role that can execute the necessary actions involved with this module (ie: invoke, put permissions)
+    '''
     invocation = invokeFunction(newLambdaName)
     groupName, groupArn = getLogGroupName(newLambdaName)
     arn = getDestinationArn(destLambdaName, destLambdaAlias)
--- maestro/dlq.py
+++ maestro/dlq.py
@@ -1,46 +1,60 @@
+#External libs
 import boto3
 import sys
 import os
 import json
 from botocore.exceptions import ClientError
 
+#Establish our boto resources
 sns_client = boto3.client('sns')
 sqs_client = boto3.client('sqs')
 sqs = boto3.resource('sqs')
 
 def get_sns_arn(topic_name):
-  list_of_topics = []
-  try:
-    existing_topics = sns_client.list_topics()
-    dumper = json.dumps(existing_topics, indent=4)
-    loader = json.loads(dumper)
-    topics = loader['Topics']
+	'''
+	Gets list of SNS topics & locates the correct one based on name provided in config file
 
-    for obj in topics:
-      for key, value in obj.items():
-        list_of_topics.append(value)
+	args:
+		topic_name: name of a valid SNS topic
+	'''
+	list_of_topics = []
+	try:
+		existing_topics = sns_client.list_topics()
+		dumper = json.dumps(existing_topics, indent=4)
+		loader = json.loads(dumper)
+		topics = loader['Topics']
 
-    for item in list_of_topics:
-      if topic_name in item:
-        return item
+		for obj in topics:
+			for key, value in obj.items():
+				list_of_topics.append(value)
 
-  except ClientError as error:
-    print(json.dumps(error.response, indent=4))
+		for item in list_of_topics:
+			if topic_name in item:
+				return item
+
+	except ClientError as error:
+		print(json.dumps(error.response, indent=4))
 
 def get_sqs_arn(sqs_name):
-  try:
-    get_queues = sqs_client.list_queues(
-                  QueueNamePrefix=sqs_name
-                )
+	'''
+	Gets lists of SQS queues & locates the correct one based on name provided in config file
 
-    dump = json.dumps(get_queues, indent=4)
-    loads = json.loads(dump)
+	args:
+		sqs_name: name of a valid SQS queue
+	'''
+	try:
+		get_queues = sqs_client.list_queues(
+									QueueNamePrefix=sqs_name
+								)
 
-    if loads['ResponseMetadata']['HTTPStatusCode'] == 200:
-      queue_urls = loads['QueueUrls'][0]
-      if len(queue_urls)>0:
-        queue = sqs.Queue(queue_urls)
-        return queue.attributes['QueueArn']
+		dump = json.dumps(get_queues, indent=4)
+		loads = json.loads(dump)
 
-  except ClientError as error:
-    print(error.response['Error']['Message'])
+		if loads['ResponseMetadata']['HTTPStatusCode'] == 200:
+			queue_urls = loads['QueueUrls'][0]
+			if len(queue_urls)>0:
+				queue = sqs.Queue(queue_urls)
+				return queue.attributes['QueueArn']
+
+	except ClientError as error:
+		print(error.response['Error']['Message'])
