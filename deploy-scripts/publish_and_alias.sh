04489a9721aae0729038215257501192431615e9
Max Moon <moon.maxwell@gmail.com>
2017-11-18 14:25:16 -0800

    removing build scripts

--- deploy-scripts/publish_and_alias.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git_sha=$(git rev-parse HEAD)
-
-docker run --rm -e AWS_ACCESS_KEY_ID='%env.AWS_ACCESS_KEY_ID%' -e AWS_SECRET_ACCESS_KEY='%env.AWS_SECRET_ACCESS_KEY%' -e AWS_DEFAULT_REGION='%env.region%' -v `pwd`:/app tunein/maestro-alpine:0.1.1 publish --version_description $git_sha %env.account%.%env.region%.%env.environment%.json
-
-docker run --rm -e AWS_ACCESS_KEY_ID='%env.AWS_ACCESS_KEY_ID%' -e AWS_SECRET_ACCESS_KEY='%env.AWS_SECRET_ACCESS_KEY%' -e AWS_DEFAULT_REGION='%env.region%' -v `pwd`:/app tunein/maestro-alpine:0.1.1 create-alias --publish %env.account%.%env.region%.%env.environment%.json
